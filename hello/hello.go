6e80635cbe1c5475f37a4a9b0989ad4962b9d904
lukblo <lukas.bloedorn@gmail.com>
2021-10-29 22:42:18 +0200

    unit tests

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	message, err := greetings.Hellos(names)
+
+	//fmt.Println(message)
+
+	// Request a greeting message.
+	//message, err = greetings.Hello("Lukas")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
 }
